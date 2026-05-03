45a5cb42c7960ff34f78acb86a3c32e9a0955ac6
Muhammad Ahmed <ahmedafzal7262@gmail.com>
2026-05-03 16:00:09 +0500

    Update BINARY FUNCTION.java

--- BINARY FUNCTION.java
+++ BINARY FUNCTION.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 class BinaryAssignment {
 
-    // 🔹 Part (b): Function returning binary as array
+    
     public static int[] toBinaryArray(int num) {
         int temp = num;
         int count = 0;
 
-        // count number of bits
+        
         while (temp > 0) {
             temp = temp / 2;
             count++;
@@ -16,7 +16,7 @@ class BinaryAssignment {
         int[] binary = new int[count];
         temp = num;
 
-        // fill array
+        
         for (int i = count - 1; i >= 0; i--) {
             binary[i] = temp % 2;
             temp = temp / 2;
@@ -25,7 +25,7 @@ class BinaryAssignment {
         return binary;
     }
 
-    // 🔹 Part (c): Function returning binary as string
+    
     public static String toBinaryString(int num) {
         int temp = num;
         String binary = "";
@@ -44,12 +44,12 @@ class BinaryAssignment {
         System.out.print("Enter a number: ");
         int a = keyb.nextInt();
 
-        // 🔹 Part (a): Direct conversion (built-in)
+        
         String builtIn = Integer.toBinaryString(a);
         System.out.println("\nPart (a) - Built-in Method:");
         System.out.println("Binary of " + a + " = " + builtIn);
 
-        // 🔹 Part (b): Using array function
+       
         int[] arrayResult = toBinaryArray(a);
         System.out.println("\nPart (b) - Binary as Array:");
         System.out.print("Binary = ");
@@ -57,7 +57,7 @@ class BinaryAssignment {
             System.out.print(bit);
         }
 
-        // 🔹 Part (c): Using string function
+        
         String stringResult = toBinaryString(a);
         System.out.println("\n\nPart (c) - Binary as String:");
         System.out.println("Binary = " + stringResult);
